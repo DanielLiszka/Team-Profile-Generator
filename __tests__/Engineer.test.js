bc95a2ce4e8202e62c1739ebf98901fdc267fd82
DanielLiszka <liszkadp@alumni.vcu.edu>
2022-05-23 14:11:39 -0400

    added engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -29,8 +29,29 @@ describe("Engineer", () => {
   it("Should return the Engineer role", () => {
     expect(testEngineer.getRole()).toBe(role);
   });
-
-
-
-  
+  it("Can set Name", () => {
+    const test_value = "Connor Anderson";
+    const test_object = new Engineer(test_value,id,email,gitHubUsername,role);
+    expect(test_object.name).toBe(test_value);
+    });
+    it("Can set id", () => {
+        const test_value = 391;
+        const test_object = new Engineer(name,test_value,email,gitHubUsername,role);
+        expect(test_object.id).toBe(test_value);
+    });
+    it("Can set email", () => {
+        const test_value = "rickylesia@gmail.com";
+        const test_object = new Engineer(name,id,test_value,gitHubUsername,role);
+        expect(test_object.email).toBe(test_value);
+    });
+    it("Can set Github Username", () => {
+        const test_value = "RickyMan";
+        const test_object = new Engineer(name,id,email,test_value,role);
+        expect(test_object.username).toBe(test_value);
+    });
+    it("Can set role", () => {
+        const test_value = "Junior Engineer";
+        const test_object = new Engineer(name,id,email,gitHubUsername,test_value);
+        expect(test_object.role).toBe(test_value);
+    });
 });
